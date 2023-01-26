e303dbbb29929eeb589c657100e44931e3055dd5
SandroIerna <iernasandro@gmail.com>
2023-01-26 18:38:53 +0100

    adds cors whitelist

--- src/server.js
+++ src/server.js
@@ -12,12 +12,32 @@ import {
 } from "./errorHandlers.js";
 import experiencesRouter from "./api/experiences/index.js";
 import usersRouter from "./api/users/index.js";
+import createHttpError from "http-errors";
 
 const server = express();
 const port = process.env.PORT;
 
+const whitelist = [
+  process.env.FE_PROD_URL,
+  process.env.MONGO_URL,
+  process.env.CLOUDINARY_URL,
+];
+
+const corsOpts = {
+  origin: (origin, corsNext) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      corsNext(null, true);
+    } else {
+      corsNext(
+        createHttpError(400, `Origin ${origin} is not in the whitelist!`)
+      );
+    }
+  },
+};
+
 // ****************************** MIDDLEWARES ******************************
-server.use(cors());
+
+server.use(cors(corsOpts));
 server.use(express.json());
 
 /* mongoose.set("strictQuery", false); */
